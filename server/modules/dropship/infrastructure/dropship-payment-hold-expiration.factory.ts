a78db6b856b8c889e9d0de3ee0ebb64d9033fdb5
cardshellz <owner@cardshellz.com>
2026-05-05 08:25:20 -0400

    feat(dropship): notify expired payment holds

--- server/modules/dropship/infrastructure/dropship-payment-hold-expiration.factory.ts
+++ server/modules/dropship/infrastructure/dropship-payment-hold-expiration.factory.ts
@@ -3,11 +3,13 @@ import {
   makeDropshipPaymentHoldExpirationLogger,
   systemDropshipPaymentHoldExpirationClock,
 } from "../application/dropship-payment-hold-expiration-service";
+import { createDropshipNotificationServiceFromEnv } from "./dropship-notification.factory";
 import { PgDropshipPaymentHoldExpirationRepository } from "./dropship-payment-hold-expiration.repository";
 
 export function createDropshipPaymentHoldExpirationServiceFromEnv(): DropshipPaymentHoldExpirationService {
   return new DropshipPaymentHoldExpirationService({
     repository: new PgDropshipPaymentHoldExpirationRepository(),
+    notificationSender: createDropshipNotificationServiceFromEnv(),
     clock: systemDropshipPaymentHoldExpirationClock,
     logger: makeDropshipPaymentHoldExpirationLogger(),
   });
