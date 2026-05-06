86721e10a8bff8e30251d1b346576141ab86c489
cardshellz <owner@cardshellz.com>
2026-05-05 21:20:31 -0400

    feat(dropship): notify vendors when orders are rejected

--- server/modules/dropship/infrastructure/dropship-order-rejection.factory.ts
+++ server/modules/dropship/infrastructure/dropship-order-rejection.factory.ts
@@ -3,11 +3,13 @@ import {
   makeDropshipOrderRejectionLogger,
   systemDropshipOrderRejectionClock,
 } from "../application/dropship-order-rejection-service";
+import { createDropshipNotificationServiceFromEnv } from "./dropship-notification.factory";
 import { PgDropshipOrderRejectionRepository } from "./dropship-order-rejection.repository";
 
 export function createDropshipOrderRejectionServiceFromEnv(): DropshipOrderRejectionService {
   return new DropshipOrderRejectionService({
     repository: new PgDropshipOrderRejectionRepository(),
+    notificationSender: createDropshipNotificationServiceFromEnv(),
     clock: systemDropshipOrderRejectionClock,
     logger: makeDropshipOrderRejectionLogger(),
   });
